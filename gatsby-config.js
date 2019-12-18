dbf565c46017d36d13b38a693c548f7795202cad
Bart Wijnants <bart.w@sparkcentral.com>
2019-12-18 07:13:41 +0100

    Fill The Head The Rest Will Follow

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Bart Wijnants' Blog`,
+        short_name: `Blog`,
+        start_url: `/`,
+        background_color: `#333333`,
+        theme_color: `#19A974`,
+        display: `standalone`,
+        icon: `src/images/logo.svg`
+      }
+    }
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
-export default () => <div>Hello world!</div>;
+export default () => <>
+<Helmet>
+      <title>Blog - Bart Wijnants</title>
+      <meta name="description" content="Bart Wijnants' Blog" />
+    </Helmet>
+<div>Hello world!</div></>;
