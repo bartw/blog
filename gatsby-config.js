2c46d6a5fb411884ed04b33dabcbf5b377cd0bdf
Bart Wijnants <bart.w@sparkcentral.com>
2019-12-18 14:09:42 +0100

    tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,15 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Bart Wijnants' Blog`,
         short_name: `Blog`,
         start_url: `/`,
-        background_color: `#333333`,
-        theme_color: `#19A974`,
+        background_color: `#000000`,
+        theme_color: `#ffffff`,
         display: `standalone`,
         icon: `src/images/logo.svg`
       }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-export default () => <>
-<Helmet>
+export default () => (
+  <>
+    <Helmet>
       <title>Blog - Bart Wijnants</title>
       <meta name="description" content="Bart Wijnants' Blog" />
     </Helmet>
-<div>Hello world!</div></>;
+    <header className="bg-black text-white p-10">
+      <h1 className="text-lg">Bart Wijnants's Blog</h1>
+    </header>
+    <main className="bg-white text-black p-10">Hello blog!</main>
+    <footer className="bg-black text-white">
+      <p className="text-xs px-10 py-5">
+        Fork me at{" "}
+        <a
+          href="https://github.com/bartw/blog"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="no-underline hover:underline"
+        >
+          GitHub
+        </a>
+      </p>
+    </footer>
+  </>
+);
